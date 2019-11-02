c88970a7ad5247c242408de612d1f554d52d08d4
jmargielewski <kuba.margielewski@gmail.com>
2019-11-02 15:13:48 +0000

    Updates

---
+++ precache-manifest.e7541f7e257762a005dde1398d0c38bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e316dbd3c626857ae7462854e70d566",
+    "url": "/Robo-PWA/index.html"
+  },
+  {
+    "revision": "eeeeac5c3aeeb535c5f4",
+    "url": "/Robo-PWA/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "95b20280ab6dd4370848",
+    "url": "/Robo-PWA/static/css/main.90ca2ce2.chunk.css"
+  },
+  {
+    "revision": "eeeeac5c3aeeb535c5f4",
+    "url": "/Robo-PWA/static/js/2.07323b84.chunk.js"
+  },
+  {
+    "revision": "95b20280ab6dd4370848",
+    "url": "/Robo-PWA/static/js/main.4f6daf63.chunk.js"
+  },
+  {
+    "revision": "78015304c106fe013a4b",
+    "url": "/Robo-PWA/static/js/runtime-main.e545f8c2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-PWA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
